bd12043bceca3352e43909757093df94386eb840
Samuel Havron <sgh7cc@virginia.edu>
2016-12-18 23:11:43 -0500

    added vundle support

--- unpack.sh
+++ unpack.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # use this script to unpack all dotfiles to proper places in an environment
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 cp .vimrc ~/
